435f91a62b97318fd25595d40e5658f99d6c54a6
MasonWang025 <mason.wang0025@gmail.com>
2020-06-02 13:21:54 -0700

    indexing resolved

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # Cache-Optimization
+
 Improving cache hit rate on MIPS GEMM (general matrix multiply) using blocking.
 
-The optimized version exploits subword parallelism and operates of 4x4 submatrices to increase hit rate.
+The optimized version operates of 4x4 submatrices to increase hit rate.
+
+## Optimized C Code
+
+Altered from page 415 of _Hennesy and Patterson Computer Organization and Design_.
+The book's example is optimized DGEMM, with incorrect indexing. The code below (found in `optimized.c`) is GEMM with fixed indexing.
+
+```
+#define N 8 // 8x8 matrix
+#define MATRIX_SIZE N*N
+#define BLOCKSIZE 4 // blocking into 4 4x4 matrices
+
+void do_block(int si, int sj, int sk, int *A, int *B, int *C)
+{
+    for (int i = si; i < si + BLOCKSIZE; ++i)
+        for (int j = sj; j < sj + BLOCKSIZE; ++j)
+        {
+            int cij = C[(i * N) + j]; /* cij = C[i][j] */
+            for (int k = sk; k < sk + BLOCKSIZE; k++)
+                cij += A[(i * N) + k] * B[(k * N) + j]; /* cij+=A[i][k]*B[k][j] */
+            C[(i * N) + j] = cij;                       /* C[i][j] = cij */
+        }
+}
+
+void matrix_multiply(int *A, int *B, int *C)
+{
+    for (int si = 0; si < N; si += BLOCKSIZE)
+        for (int sj = 0; sj < N; sj += BLOCKSIZE)
+            for (int sk = 0; sk < N; sk += BLOCKSIZE)
+                do_block(si, sj, sk, A, B, C);
+}
+```
--- optimized.c
+++ optimized.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define N 8         // 8x8 matrix
+#define N 8 // 8x8 matrix
 #define MATRIX_SIZE N*N
 #define BLOCKSIZE 4 // blocking into 4 4x4 matrices
+
+// M1 and M2 initalized in main
+int product[MATRIX_SIZE];
 
 // MIPS print_matrix procedure
 void print_matrix(int *C)
@@ -15,12 +18,12 @@ void print_matrix(int *C)
         printf("Row %d: ", (i + 1));
         // MIPS print_vector procedure
         for (int j = 0; j < N; j++)
-            printf("%d ", C[j+i*N]);
+            printf("%d ", C[(i * N) + j]); // (i*N) is row number * row size
         printf("\n");
     }
 }
 
-// UNOPTIMIZED DGEMM:
+// UNOPTIMIZED DGEMM (exploiting subword parallelism):
 // for (int j = 0; j < n; ++j) {
 // int cij = C[i+j*n]; /* cij = C[i][j] */
 // for( int k = 0; k < n; k++ )
@@ -43,20 +46,17 @@ void do_block(int si, int sj, int sk, int *A, int *B, int *C)
     for (int i = si; i < si + BLOCKSIZE; ++i)
         for (int j = sj; j < sj + BLOCKSIZE; ++j)
         {
-            int cij = C[i + j * N]; /* cij = C[i][j] */
+            int cij = C[(i * N) + j]; /* cij = C[i][j] */
             for (int k = sk; k < sk + BLOCKSIZE; k++)
-                cij += A[i + k * N] * B[k + j * N]; /* cij+=A[i][k]*B[k][j] */
-            C[i + j * N] = cij;                     /* C[i][j] = cij */
+                cij += A[(i * N) + k] * B[(k * N) + j]; /* cij+=A[i][k]*B[k][j] */
+            C[(i * N) + j] = cij;                       /* C[i][j] = cij */
         }
 }
 
 void matrix_multiply(int *A, int *B, int *C)
 {
-    printf("sfdsfsdf\n");
-    print_matrix(A);
-    printf("sfdsfsdf\n");
-    for (int sj = 0; sj < N; sj += BLOCKSIZE)
-        for (int si = 0; si < N; si += BLOCKSIZE)
+    for (int si = 0; si < N; si += BLOCKSIZE)
+        for (int sj = 0; sj < N; sj += BLOCKSIZE)
             for (int sk = 0; sk < N; sk += BLOCKSIZE)
                 do_block(si, sj, sk, A, B, C);
 }
@@ -64,16 +64,16 @@ void matrix_multiply(int *A, int *B, int *C)
 int main()
 {
     // defined in .data
-    int M[MATRIX_SIZE] = {
-        1, 2, 1, 1, 0, 0, 1, 2,
-        0, 4, 0, 1, 7, 4, 3, 3,
-        2, 3, 4, 1, 6, 1, 9, 1,
-        1, 9, 7, 0, 7, 8, 1, 2,
-        2, 1, 8, 6, 8, 0, 1, 1,
-        6, 1, 8, 1, 0, 0, 1, 7,
-        1, 2, 9, 1, 2, 7, 1, 9,
-        6, 1, 1, 1, 6, 2, 2, 1};
     int M1[MATRIX_SIZE] =
+        {1, 2, 1, 1, 0, 0, 1, 2,
+         0, 4, 0, 1, 7, 4, 3, 3,
+         2, 3, 4, 1, 6, 1, 9, 1,
+         1, 9, 7, 0, 7, 8, 1, 2,
+         2, 1, 8, 6, 8, 2, 1, 1,
+         6, 1, 8, 1, 0, 2, 1, 7,
+         1, 2, 9, 1, 2, 7, 1, 9,
+         6, 1, 1, 1, 6, 2, 2, 1};
+    int M2[MATRIX_SIZE] =
         {8, 8, 9, 1, 7, 1, 2, 3,
          1, 3, 0, 0, 1, 2, 4, 5,
          5, 2, 5, 7, 2, 8, 5, 7,
@@ -83,10 +83,8 @@ int main()
          2, 3, 5, 1, 6, 6, 2, 3,
          7, 0, 1, 1, 2, 4, 6, 4};
 
-    int product[MATRIX_SIZE];
-
     // MIPS: $a1 * $a2 -> $a0
-    matrix_multiply(product, M, M1);
+    matrix_multiply(M1, M2, product);
 
     print_matrix(product);
 
